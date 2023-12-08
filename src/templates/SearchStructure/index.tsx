5df9079d8d2d9027a66325654a7fa0a583089d15
igorbaio <igorbaiosoares@gmail.com>
2023-12-08 18:32:09 -0300

    added search function

--- src/templates/SearchStructure/index.tsx
+++ src/templates/SearchStructure/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Image,
   Platform,
@@ -24,17 +24,38 @@ import { useFilters } from "../../stores/Filters";
 import { colors } from "../../Styles/theme";
 import { MaterialIcons } from '@expo/vector-icons';
 import { useUser } from "../../stores/User";
+import { GenericInput } from "../../Molecules/GenericInput";
 
 
 export function SearchStructure({ navigation }: any) {
-  const { nationality, setNationality } = useFilters(state => state);
   const { users } = useUser((state) => state);
+  const [search, setSearch] = React.useState<string>("");
+  const [searchResult, setSearchResult] = React.useState<any[]>(users.filter((user, index) => index < 10) || []);
+  const { user, setUser } = useUser(state => state)
+  const [nationality, setNationality] = React.useState<string>(user?.country || "");
+  console.log('nationality', nationality)
+
+  const getFilter = () => {
+    let usersAux = users.filter((userList, index) =>
+    (userList.displayName.toLowerCase().includes(search.toLowerCase()) ||
+      userList.services.join().toLowerCase().includes(search.toLowerCase()))
+    )
+    if (!!nationality) usersAux = usersAux.filter(userList => userList.country === nationality)
+    usersAux = usersAux.filter((userList, index) => index < 10)
+    setSearchResult(usersAux);
+  }
+
+  useEffect(() => {
+    getFilter()
+  }, [search, nationality])
+
   return (
     <SearchContainer>
       <InputContainer>
-        <InputSearch placeholder="Buscar" />
+        <GenericInput placeholder="Buscar" onChangeText={(e: string) => setSearch(e)} value={search} />
         <InputCheckBoxContainer onPress={() => {
-          setNationality(!nationality)
+          if (!!nationality) setNationality("")
+          else setNationality(user.country)
         }}>
           <InputCheckBox isSelected={nationality}  >
             {nationality && <MaterialIcons name="done" size={18} color={colors.whitePrimary} />}
@@ -44,8 +65,8 @@ export function SearchStructure({ navigation }: any) {
         </InputCheckBoxContainer>
       </InputContainer>
       <FeedContainer>
-        <GalleryContainer>
-          <Gallery items={users || []} navigation={navigation} />
+        <GalleryContainer >
+          <Gallery items={searchResult} navigation={navigation} />
         </GalleryContainer>
       </FeedContainer>
     </SearchContainer>
